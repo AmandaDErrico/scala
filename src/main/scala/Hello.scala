6a8d7dafabf4d16c33a6ea4adf01358d71e99372
Amanda D'Errico <amanda.derrico@datapassports.com>
2019-12-07 23:38:42 -0500

    another test

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,5 @@
 
 // to allow IntelliJ and Scala to connect to main program, must extends App
 object Hello extends App {
-  println("Hello World Amanda!")
+  println("Hello World Amanda, testing!")
 }
