eb106b722652891e4ef7f24735ab604347b22d28
Amanda D'Errico <amanda.derrico@datapassports.com>
2019-12-09 13:16:25 -0500

    added or function

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -2,9 +2,16 @@
 // to allow IntelliJ and Scala to connect to main program, must extends App
 object Hello extends App {
   println("Hello World Amanda, testing!")
+
+  // AND
   def and(x:Boolean, y: =>Boolean) = if(x) y else false
   val x = true
   val y = true
   println("Test result: " + and(x, y))
 
+  // OR
+  def or(x:Boolean, y: =>Boolean) = if(x) true else y
+  val a = false
+  val b = false
+  println("Test result: " + and(a, b))
 }
