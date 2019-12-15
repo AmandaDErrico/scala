03d97f77e39aef0de7921bfc66e602e58cc227dd
Amanda D'Errico <amanda.derrico@datapassports.com>
2019-12-15 17:47:37 -0500

    finished recursion file with main class

--- src/main/scala/recursion/factorial.scala
+++ src/main/scala/recursion/factorial.scala
@@ -1,13 +1,17 @@
 package recursion
 
 object factorial {
-//  def factorial(n: Int): Int = {
-//    // factorial: (n: Int)Int, res0: Int = 24
-//    def loop(acc: Int, n: Int): Int =
-//      if (n == 0) acc
-//      else loop(acc*n, n-1)
-//     loop(1, n)
-//  }
-//  factorial(4)
-  println("hello recursion")
+  def myFactorial(n: Int): Int = {
+    // factorial: (n: Int)Int, res0: Int = 24
+    def loop(acc: Int, n: Int): Int =
+      if (n == 0) acc
+      else loop(acc*n, n-1)
+     loop(1, n) // return statement
+  }
+  // representing static main class, can also put object factorial extends App
+  def main(args: Array[String]) {
+    println(factorial.myFactorial(4))
+  }
 }
+
+
