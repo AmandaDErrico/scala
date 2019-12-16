c12157cb635e8d8eb3da33134682975d4ca54045
Amanda D'Errico <amanda.derrico@datapassports.com>
2019-12-15 19:08:00 -0500

    completed sum function with CBN as a function

--- src/main/scala/recursion/factorial.scala
+++ src/main/scala/recursion/factorial.scala
@@ -8,9 +8,20 @@ object factorial {
       else loop(acc*n, n-1)
      loop(1, n) // return statement
   }
+
+  def sum(f: Int => Int, a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (a > b) acc
+      else loop(a+1, f(a) + acc)
+    }
+    // everything is applied to function x*x
+    loop(a, 0)
+  }
+
   // representing static main class, can also put object factorial extends App
   def main(args: Array[String]) {
     println(factorial.myFactorial(4))
+    println(factorial.sum(x => x * x, 3, 5)) // res1 = 50
   }
 }
 
